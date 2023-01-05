5a2a5efa50b36d473def6ae82550bd6ef5b6951c
Cristóbal Mardones Bucarey <cmardone@gmail.com>
2023-01-04 23:27:45 -0300

    Up to video Nº 15 finished

--- src/bases/CounterEffect.tsx
+++ src/bases/CounterEffect.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react';
+import { gsap } from 'gsap';
 
 const MAXIMUM_COUNT = 10;
 
@@ -8,6 +9,9 @@ export const CounterEffect = () => {
   useEffect(() => {
     if (counter < MAXIMUM_COUNT) return;
     console.log('%cMaximum value reached', 'color: red; background-color: black;');
+    gsap
+      .to('h2', { y: -10, duration: 0.2, ease: 'ease.out' })
+      .then(() => gsap.to('h2', { y: 0, duration: 1, ease: 'bounce.out' }));
   }, [counter]);
 
   const addClickHandle = () => {
@@ -16,7 +20,8 @@ export const CounterEffect = () => {
 
   return (
     <>
-      <h1>CounterEffect: {counter}</h1>
+      <h1>CounterEffect:</h1>
+      <h2>{counter}</h2>
       <button type='button' onClick={addClickHandle}>
         +1
       </button>
